073ae3d649cc5590fa35269bcd0dc5a118b55c37
faroukbouraoui <faroukbr050@gmail.com>
2023-08-10 22:02:18 +0100

    adding beautifulsoup

--- check_website.py
+++ check_website.py
@@ -1,5 +1,15 @@
 import requests
-producturl="*****"
+import bs4
+
+producturl="******"
 
 res =  requests.get(producturl, timeout=20)
-print(res)
+#print(res)
+if res.status_code != 200:
+    pass
+
+
+
+soup = bs4.BeautifulSoup(res.text,'html.parser')
+element = soup.select('body > main > div.featuredSection > div.small-12.columns.discover-block.p-0 > div > div > div > div.register-block > li.menuItem.register.btn.btn-blue > a') #put your selector here
+print(element)
